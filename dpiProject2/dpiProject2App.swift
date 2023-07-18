f03e643ded05f1de32c82c14a482f15f1bd86ef2
Jakub Majewski <jakub.majewski@softwarehut.com>
2023-07-18 16:36:54 +0200

    Init Xcode Project

---
+++ dpiProject2/dpiProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  dpiProject2App.swift
+//  dpiProject2
+//
+//  Created by Jakub Majewski on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct dpiProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
